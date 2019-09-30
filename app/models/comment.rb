f6b368e6cd787325f07c94ff98938ea6e516ed79
Neoism <ami@seooiyoiyonoMacBook-Pro.local>
2019-09-30 18:40:47 +0900

    add validates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
   validates :comment, presence: true
+  validates :user_id, presence: true
+  validates :tweet_id, presence: true
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,7 +7,7 @@ class Tweet < ApplicationRecord
     presence: true,
     length: { maximum: 200 }
 
-    def favorited_by?(user)
-      favorites.where(user_id: user.id).exists?
-    end
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
